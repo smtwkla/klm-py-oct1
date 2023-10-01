8d380834b067dfed7064c018e15f90dd4d3b982a
KLA <smtwkla@gmail.com>
2023-10-01 14:02:49 +0530

    determinant calc works with 2x2

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def check_square_matrix(m):
+    rows = len(m)
+    for row in m:
+        if len(row) != rows:
+            return False
+    return True
+
+
+def get_sub_matrix(matrix, rows, cols):
+    m = []
+    for i in rows:
+        r = [matrix[i][x] for x in cols]
+        m.append(r)
+    return m
 
 
 def calc_determinant(a):
@@ -6,16 +20,16 @@ def calc_determinant(a):
         d = a[0][0] * a[1][1] - a[1][0] * a[0][1]
         return d
     else:
-        # calculate sub-matrix for each element in row[0]
-        # call calc_determinant recursively
-        # add determinant with sum
-        pass
+        det_sum = 0
+        sign = 1
+
+        for i in range(order):
+            r = []
+            c = []
+            sub_m = get_sub_matrix(a, r, c)
+            d = calc_determinant(sub_m)
+            det_sum += a[i] * d * sign
 
 
-def check_square_matrix(m):
-    rows = len(m)
-    for row in m:
-        if len(row) != rows:
-            return False
-    return True
+
 
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 
-from main import check_square_matrix
+from main import check_square_matrix, get_sub_matrix, calc_determinant
 
 
 def test_check_square_matrix():
@@ -13,5 +13,21 @@ def test_check_square_matrix():
     assert not check_square_matrix(test_matrix)
 
 
+def test_sub_matrix():
+    m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    g = get_sub_matrix(m,[1,2],[0,2])
+    assert g == [[4, 6], [7, 9]]
+
+
+def test_calc_determinant():
+    m = [[1, 2], [4, 5]]
+    assert calc_determinant(m) == -3
+
+    m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert calc_determinant(m) == 0
+
+
 if __name__ == "__main__":
     test_check_square_matrix()
+    test_sub_matrix()
+    test_calc_determinant()
