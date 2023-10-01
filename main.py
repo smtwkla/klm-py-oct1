06d6eb2e0de20d1b31e8744b2d806a322b91a256
KLA <smtwkla@gmail.com>
2023-10-01 13:38:35 +0530

    sudo code for determinant calc

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 
+def calc_determinant(a):
+    order = len(a[0])
+    if order == 2:
+        d = a[0][0] * a[1][1] - a[1][0] * a[0][1]
+        return d
+    else:
+        # calculate sub-matrix for each element in row[0]
+        # call calc_determinant recursively
+        # add determinant with sum
+        pass
+
+
 def check_square_matrix(m):
     rows = len(m)
     for row in m:
