42ee80dab42c9ac41c56a83f4b5e1a58ac9ebe5b
KLA <smtwkla@gmail.com>
2023-10-01 13:26:20 +0530

    seperated test py

--- main.py
+++ main.py
@@ -7,17 +7,3 @@ def check_square_matrix(m):
             return False
     return True
 
-
-def test_check_square_matrix():
-    test_matrix = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
-    assert check_square_matrix(test_matrix)
-
-    test_matrix = [[1, 2, 3], [1, 2, 3], [1, 2]]
-    assert not check_square_matrix(test_matrix)
-
-    test_matrix = [[1, 2], [1, 2], [1, 2]]
-    assert not check_square_matrix(test_matrix)
-
-
-if __name__ == '__main__':
-    test_check_square_matrix()
